8102e24fc85b3805fa2451fd224241207b376610
N Smith <digitaldoesnthurt@gmail.com>
2017-07-20 16:23:00 -0700

    another email alert dubug

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,7 +7,7 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 
 Rails.application.config.before_configuration do
-	env_file = File.join(Rails.root, 'config', 'local_env.yml')
+	env_file = File.join(Rails.root, 'config', 'initializers','local_env.yml')
 	YAML.load(File.open(env_file)).each do |key, value|
     	ENV[key.to_s] = value
 	end if File.exists?(env_file)
