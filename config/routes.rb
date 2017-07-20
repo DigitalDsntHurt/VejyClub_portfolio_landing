48ac0a08d6191e3434e117134d4535c30dec06ef
N Smith <digitaldoesnthurt@gmail.com>
2017-07-19 23:54:17 -0700

    set homepage

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :leads
 	get 'grayscales/index'
 	get 'grayscales/form_land'
+	root 'grayscales#form_land'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
